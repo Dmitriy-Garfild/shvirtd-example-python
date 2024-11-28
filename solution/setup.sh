869595f8d847006fe448ef6e25e799bbfc45d7c1
Garfild <sameory@gmail.com>
2024-11-28 19:22:31 +0300

    file bush

---
+++ solution/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+REPO_URL="https://github.com/Dmitriy-Garfild/shvirtd-example-python.git"  
+DEST_DIR="/opt/shvirtd-example-python"  
+# Скачивание репозитория
+if [ ! -d "$DEST_DIR" ]; then
+  echo "Клонируем репозиторий в $DEST_DIR..."
+  git clone "$REPO_URL" "$DEST_DIR"
+else
+  echo "Репозиторий уже существует в $DEST_DIR. Обновляем "
+  cd "$DEST_DIR" || exit
+  git pull origin main  
+fi
+
+
+cd "$DEST_DIR" 
+
+
+
+docker-compose up -d
+
+
